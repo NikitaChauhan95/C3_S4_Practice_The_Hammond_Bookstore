6391284208ac9b6a85f8c98c86270aa753800b60
Nikita Chauhan <nikitachauhan64@gmail.com>
2022-08-27 23:54:28 +0530

    created the getter and setter method

--- src/main/java/com/bookstore/Book.java
+++ src/main/java/com/bookstore/Book.java
@@ -17,10 +17,42 @@ public class Book {
     }
 
     public Book(long isbnNumber, String title, String description, double price) {
-        K
+
         this.isbnNumber = isbnNumber;
         this.title = title;
         this.description = description;
+        this.price = price;
+    }
+
+    public long getIsbnNumber() {
+        return isbnNumber;
+    }
+
+    public void setIsbnNumber(long isbnNumber) {
+        this.isbnNumber = isbnNumber;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
         this.price = price;
     }
 }

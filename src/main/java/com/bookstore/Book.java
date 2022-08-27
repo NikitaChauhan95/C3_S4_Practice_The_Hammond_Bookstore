9da6b5fe6e522b95b91e4b7752248a320501f0a1
Nikita Chauhan <nikitachauhan64@gmail.com>
2022-08-27 23:47:51 +0530

    created the book class

---
+++ src/main/java/com/bookstore/Book.java
@@ -0,0 +1,19 @@
+/*
+ *Author Name: Nikita Chauhan
+ *Date: 27-08-2022
+ *Created With: IntelliJ IDEA Community Edition
+ */
+
+
+package com.bookstore;
+
+public class Book {
+   /* private long isbnNumber;
+    private String title;
+    private String description;
+    private double price;
+
+    */
+
+
+}
